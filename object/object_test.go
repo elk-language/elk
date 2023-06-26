2f22a7dff39cbd89f1a78a3c34bc1f8e6d3425c5
Mateusz Drewniak <matmg24@gmail.com>
2023-06-26 13:56:29 +0200

    Add a benchmark testing custom interfaces

--- object/object_test.go
+++ object/object_test.go
@@ -6,6 +6,81 @@ import (
 	"github.com/google/go-cmp/cmp"
 )
 
+// BENCHMARK: Try implementing this idea and measure
+// the difference once the VM is fully operational.
+//
+// func Noop(...any) {}
+
+// // Mimics the structure of the Go's interface.
+// type ElkInterface struct {
+// 	// itabs are allocated in non garbage collected memory
+// 	// so using uintptr should be safe
+// 	itab uintptr
+// 	data unsafe.Pointer
+// }
+
+// const (
+// 	UINT64_BITMASK  uintptr = 0b00
+// 	INT64_BITMASK   uintptr = 0b01
+// 	FLOAT64_BITMASK uintptr = 0b10
+// )
+
+// func Int64ToInterface(i Int64) ElkInterface {
+// 	return ElkInterface{
+// 		itab: uintptr(i),
+// 		data: unsafe.Pointer(INT64_BITMASK),
+// 	}
+// }
+
+// func (e ElkInterface) IsInt64() bool {
+// 	return uintptr(e.data) == INT64_BITMASK
+// }
+
+// func (e ElkInterface) IsUInt64() bool {
+// 	return uintptr(e.data) == UINT64_BITMASK
+// }
+
+// func (e ElkInterface) IsFloat64() bool {
+// 	return uintptr(e.data) == FLOAT64_BITMASK
+// }
+
+// func (e ElkInterface) IsRefValue() bool {
+// 	return uintptr(e.data) > FLOAT64_BITMASK
+// }
+
+// func (e ElkInterface) ToInt64() Int64 {
+// 	return Int64(e.itab)
+// }
+
+// func (e ElkInterface) ToValue() Value {
+// 	if !e.IsRefValue() {
+// 		return nil
+// 	}
+// 	return *(*Value)(unsafe.Pointer(&e))
+// }
+
+// func BenchmarkEfaceScalar(b *testing.B) {
+// 	var Uint UInt32
+// 	b.Run("uint32", func(b *testing.B) {
+// 		for i := 0; i < b.N; i++ {
+// 			Uint = UInt32(i)
+// 		}
+// 	})
+// 	var Eface Value
+// 	b.Run("eface32", func(b *testing.B) {
+// 		for i := 0; i < b.N; i++ {
+// 			Eface = UInt32(i)
+// 		}
+// 	})
+// 	var ElkIface ElkInterface
+// 	b.Run("elkInterface", func(b *testing.B) {
+// 		for i := 0; i < b.N; i++ {
+// 			ElkIface = Int64ToInterface(Int64(i))
+// 		}
+// 	})
+// 	Noop(Uint, Eface, ElkIface)
+// }
+
 func TestObjectInspect(t *testing.T) {
 	tests := map[string]struct {
 		obj  *Object
