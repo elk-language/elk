e63c595445a5c5bdad3ac83c3d01c69bc80ee7e8
Mateusz Drewniak <matmg24@gmail.com>
2023-12-29 00:45:15 +0100

    Add tests for nested loops

--- vm/vm_source_control_flow_test.go
+++ vm/vm_source_control_flow_test.go
@@ -125,6 +125,23 @@ func TestVMSource_While(t *testing.T) {
 			`,
 			wantStackTop: value.SmallInt(15),
 		},
+		"nested with break": {
+			source: `
+				j := 0
+				while true
+					j += 1
+					i := 0
+					while true
+						break if i >= 5
+						i += 1
+						println j.to_string + ":" + i.to_string
+					end
+					break if j >= 5
+				end
+			`,
+			wantStdout:   "1:1\n1:2\n1:3\n1:4\n1:5\n2:1\n2:2\n2:3\n2:4\n2:5\n3:1\n3:2\n3:3\n3:4\n3:5\n4:1\n4:2\n4:3\n4:4\n4:5\n5:1\n5:2\n5:3\n5:4\n5:5\n",
+			wantStackTop: value.Nil,
+		},
 		"return a value with break": {
 			source: `
 				a := 0
