6fd7c049357a0e961f3b6501c29ca3a06bed5e80
Mateusz Drewniak <matmg24@gmail.com>
2024-03-04 23:01:18 +0100

    Refactor the regex transpiler

--- regex/transpile.go
+++ regex/transpile.go
@@ -54,171 +54,80 @@ func (t *transpiler) transpileNode(node ast.Node) {
 	case *ast.ConcatenationNode:
 		t.concatenation(n)
 	case *ast.ZeroOrOneQuantifierNode:
-		t.transpileNode(n.Regex)
-		if n.Alt {
-			t.Buffer.WriteString(`??`)
-		} else {
-			t.Buffer.WriteRune('?')
-		}
+		t.zeroOrOneQuantifier(n)
 	case *ast.ZeroOrMoreQuantifierNode:
-		t.transpileNode(n.Regex)
-		if n.Alt {
-			t.Buffer.WriteString(`*?`)
-		} else {
-			t.Buffer.WriteRune('*')
-		}
+		t.zeroOrMoreQuantifier(n)
 	case *ast.OneOrMoreQuantifierNode:
-		t.transpileNode(n.Regex)
-		if n.Alt {
-			t.Buffer.WriteString(`+?`)
-		} else {
-			t.Buffer.WriteRune('+')
-		}
+		t.oneOrMoreQuantifier(n)
 	case *ast.NQuantifierNode:
-		t.transpileNode(n.Regex)
-		t.Buffer.WriteRune('{')
-		t.Buffer.WriteString(n.N)
-		t.Buffer.WriteRune('}')
-		if n.Alt {
-			t.Buffer.WriteRune('?')
-		}
+		t.nQuantifier(n)
 	case *ast.NMQuantifierNode:
-		t.transpileNode(n.Regex)
-		t.Buffer.WriteRune('{')
-		if n.N == "" {
-			t.Buffer.WriteRune('0')
-		} else {
-			t.Buffer.WriteString(n.N)
-		}
-		t.Buffer.WriteRune(',')
-		if n.M != "" {
-			t.Buffer.WriteString(n.M)
-		}
-		t.Buffer.WriteRune('}')
-		if n.Alt {
-			t.Buffer.WriteRune('?')
-		}
-	case *ast.CharNode:
-		t.Buffer.WriteRune(n.Value)
-	case *ast.GroupNode:
-		t.Buffer.WriteRune('(')
-		t.transpileNode(n.Regex)
-		t.Buffer.WriteRune(')')
+		t.nmQuantifier(n)
 	case *ast.MetaCharEscapeNode:
-		t.Buffer.WriteRune('\\')
-		t.Buffer.WriteRune(n.Value)
+		t.metaCharEscape(n)
+	case *ast.GroupNode:
+		t.group(n)
+	case *ast.CharClassNode:
+		t.charClass(n)
 	case *ast.QuotedTextNode:
-		t.Buffer.WriteString(`\Q`)
-		t.Buffer.WriteString(n.Value)
-		t.Buffer.WriteString(`\E`)
+		t.quotedText(n)
+	case *ast.CharNode:
+		t.char(n)
 	case *ast.CaretEscapeNode:
-		t.Buffer.WriteString(`\x{`)
-		fmt.Fprintf(&t.Buffer, "%x", asciiLetterIndex(n.Value))
-		t.Buffer.WriteString(`}`)
+		t.caretEscape(n)
 	case *ast.UnicodeEscapeNode:
-		t.Buffer.WriteString(`\x{`)
-		t.Buffer.WriteString(n.Value)
-		t.Buffer.WriteString(`}`)
+		t.unicodeEscape(n)
 	case *ast.HexEscapeNode:
-		t.Buffer.WriteString(`\x{`)
-		t.Buffer.WriteString(n.Value)
-		t.Buffer.WriteString(`}`)
+		t.hexEscape(n)
 	case *ast.OctalEscapeNode:
-		t.Buffer.WriteRune('\\')
-		t.Buffer.WriteString(fmt.Sprintf("%03s", n.Value))
+		t.octalEscape(n)
 	case *ast.UnicodeCharClassNode:
-		t.Buffer.WriteRune('\\')
-		if n.Negated {
-			t.Buffer.WriteRune('P')
-		} else {
-			t.Buffer.WriteRune('p')
-		}
-
-		t.Buffer.WriteRune('{')
-		t.Buffer.WriteString(n.Value)
-		t.Buffer.WriteRune('}')
+		t.unicodeCharClass(n)
 	case *ast.BellEscapeNode:
-		t.Buffer.WriteString(`\a`)
+		t.bellEscape()
 	case *ast.FormFeedEscapeNode:
-		t.Buffer.WriteString(`\f`)
+		t.formFeedEscape()
 	case *ast.TabEscapeNode:
-		t.Buffer.WriteString(`\t`)
+		t.tabEscape()
 	case *ast.NewlineEscapeNode:
-		t.Buffer.WriteString(`\n`)
+		t.newlineEscape()
 	case *ast.CarriageReturnEscapeNode:
-		t.Buffer.WriteString(`\r`)
+		t.carriageReturnEscape()
 	case *ast.StartOfStringAnchorNode:
-		t.Buffer.WriteRune('^')
+		t.startOfStringAnchor()
 	case *ast.EndOfStringAnchorNode:
-		t.Buffer.WriteRune('$')
+		t.endOfStringAnchor()
 	case *ast.AbsoluteStartOfStringAnchorNode:
-		t.Buffer.WriteString(`\A`)
+		t.absoluteStartOfStringAnchor()
 	case *ast.AbsoluteEndOfStringAnchorNode:
-		t.Buffer.WriteString(`\z`)
+		t.absoluteEndOfStringAnchor()
 	case *ast.WordBoundaryAnchorNode:
-		t.Buffer.WriteString(`\b`)
+		t.wordBoundaryAnchor()
 	case *ast.NotWordBoundaryAnchorNode:
-		t.Buffer.WriteString(`\B`)
+		t.notWordBoundaryAnchor()
 	case *ast.WordCharClassNode:
-		if t.AsciiMode {
-			t.Buffer.WriteString(`\w`)
-		} else {
-			t.Buffer.WriteString(`[\p{L}\p{N}_]`)
-		}
+		t.wordCharClass(false)
 	case *ast.NotWordCharClassNode:
-		if t.AsciiMode {
-			t.Buffer.WriteString(`\W`)
-		} else {
-			t.Buffer.WriteString(`[^\p{L}\p{N}_]`)
-		}
+		t.notWordCharClass(false)
 	case *ast.DigitCharClassNode:
-		if t.AsciiMode {
-			t.Buffer.WriteString(`\d`)
-		} else {
-			t.Buffer.WriteString(`\p{Nd}`)
-		}
+		t.digitCharClass()
 	case *ast.NotDigitCharClassNode:
-		if t.AsciiMode {
-			t.Buffer.WriteString(`\D`)
-		} else {
-			t.Buffer.WriteString(`\P{Nd}`)
-		}
+		t.notDigitCharClass()
 	case *ast.WhitespaceCharClassNode:
-		if t.AsciiMode {
-			t.Buffer.WriteString(`\s`)
-		} else {
-			t.Buffer.WriteString(`[\p{Z}\t\v]`)
-		}
+		t.whitespaceCharClass(false)
 	case *ast.NotWhitespaceCharClassNode:
-		if t.AsciiMode {
-			t.Buffer.WriteString(`\S`)
-		} else {
-			t.Buffer.WriteString(`[^\p{Z}\t\v]`)
-		}
+		t.notWhitespaceCharClass()
 	case *ast.HorizontalWhitespaceCharClassNode:
-		if t.AsciiMode {
-			t.Buffer.WriteString(`[\t ]`)
-		} else {
-			t.Buffer.WriteString(`[\t\p{Zs}]`)
-		}
+		t.horizontalWhitespaceCharClass()
 	case *ast.NotHorizontalWhitespaceCharClassNode:
-		if t.AsciiMode {
-			t.Buffer.WriteString(`[^\t ]`)
-		} else {
-			t.Buffer.WriteString(`[^\t\p{Zs}]`)
-		}
+		t.notHorizontalWhitespaceCharClass()
 	case *ast.VerticalWhitespaceCharClassNode:
-		if t.AsciiMode {
-			t.Buffer.WriteString(`[\n\x0b\f\r]`)
-		} else {
-			t.Buffer.WriteString(`[\n\x0b\f\r\x85\x{2028}\x{2029}]`)
-		}
+		t.verticalWhitespaceCharClass()
 	case *ast.NotVerticalWhitespaceCharClassNode:
-		if t.AsciiMode {
-			t.Buffer.WriteString(`[^\n\x0b\f\r]`)
-		} else {
-			t.Buffer.WriteString(`[^\n\x0b\f\r\x85\x{2028}\x{2029}]`)
-		}
+		t.notVerticalWhitespaceCharClass()
+	case *ast.AnyCharClassNode:
+		t.anyCharClass()
+	case nil:
 	default:
 		t.Errors.Add(
 			fmt.Sprintf("compilation of this node has not been implemented: %T", node),
@@ -231,4 +140,368 @@ func (t *transpiler) concatenation(node *ast.ConcatenationNode) {
 	for _, element := range node.Elements {
 		t.transpileNode(element)
 	}
+}
+
+func (t *transpiler) zeroOrOneQuantifier(node *ast.ZeroOrOneQuantifierNode) {
+	t.transpileNode(node.Regex)
+	if node.Alt {
+		t.Buffer.WriteString(`??`)
+	} else {
+		t.Buffer.WriteRune('?')
+	}
+}
+
+func (t *transpiler) zeroOrMoreQuantifier(node *ast.ZeroOrMoreQuantifierNode) {
+	t.transpileNode(node.Regex)
+	if node.Alt {
+		t.Buffer.WriteString(`*?`)
+	} else {
+		t.Buffer.WriteRune('*')
+	}
+}
+
+func (t *transpiler) oneOrMoreQuantifier(node *ast.OneOrMoreQuantifierNode) {
+	t.transpileNode(node.Regex)
+	if node.Alt {
+		t.Buffer.WriteString(`+?`)
+	} else {
+		t.Buffer.WriteRune('+')
+	}
+}
+
+func (t *transpiler) nQuantifier(node *ast.NQuantifierNode) {
+	t.transpileNode(node.Regex)
+	t.Buffer.WriteRune('{')
+	t.Buffer.WriteString(node.N)
+	t.Buffer.WriteRune('}')
+	if node.Alt {
+		t.Buffer.WriteRune('?')
+	}
+}
+
+func (t *transpiler) nmQuantifier(node *ast.NMQuantifierNode) {
+	t.transpileNode(node.Regex)
+	t.Buffer.WriteRune('{')
+	if node.N == "" {
+		t.Buffer.WriteRune('0')
+	} else {
+		t.Buffer.WriteString(node.N)
+	}
+	t.Buffer.WriteRune(',')
+	if node.M != "" {
+		t.Buffer.WriteString(node.M)
+	}
+	t.Buffer.WriteRune('}')
+	if node.Alt {
+		t.Buffer.WriteRune('?')
+	}
+}
+
+func (t *transpiler) char(node *ast.CharNode) {
+	t.Buffer.WriteRune(node.Value)
+}
+
+func (t *transpiler) group(node *ast.GroupNode) {
+	t.Buffer.WriteRune('(')
+	if len(node.Flags) > 0 {
+		// with flags
+		t.Buffer.WriteRune('?')
+		t.Buffer.WriteString(node.Flags)
+		if node.Regex != nil {
+			// with flags and content
+			t.Buffer.WriteRune(':')
+			t.transpileNode(node.Regex)
+		}
+		t.Buffer.WriteRune(')')
+		return
+	}
+
+	if len(node.Name) > 0 {
+		// named
+		t.Buffer.WriteString(`?P<`)
+		t.Buffer.WriteString(node.Name)
+		t.Buffer.WriteRune('>')
+	} else if node.NonCapturing {
+		// non capturing
+		t.Buffer.WriteString(`?:`)
+	}
+
+	t.transpileNode(node.Regex)
+	t.Buffer.WriteRune(')')
+}
+
+func (t *transpiler) charClass(node *ast.CharClassNode) {
+	t.Buffer.WriteRune('[')
+	if node.Negated {
+		t.Buffer.WriteRune('^')
+	}
+
+	for _, element := range node.Elements {
+		t.charClassElement(element)
+	}
+
+	t.Buffer.WriteRune(']')
+}
+
+func (t *transpiler) charClassElement(node ast.CharClassElementNode) {
+	switch n := node.(type) {
+	case *ast.CharRangeNode:
+		t.charRange(n)
+	case *ast.NamedCharClassNode:
+		t.namedCharClass(n)
+	case *ast.MetaCharEscapeNode:
+		t.metaCharEscape(n)
+	case *ast.CharNode:
+		t.char(n)
+	case *ast.CaretEscapeNode:
+		t.caretEscape(n)
+	case *ast.UnicodeEscapeNode:
+		t.unicodeEscape(n)
+	case *ast.HexEscapeNode:
+		t.hexEscape(n)
+	case *ast.OctalEscapeNode:
+		t.octalEscape(n)
+	case *ast.UnicodeCharClassNode:
+		t.unicodeCharClass(n)
+	case *ast.BellEscapeNode:
+		t.bellEscape()
+	case *ast.FormFeedEscapeNode:
+		t.formFeedEscape()
+	case *ast.TabEscapeNode:
+		t.tabEscape()
+	case *ast.NewlineEscapeNode:
+		t.newlineEscape()
+	case *ast.CarriageReturnEscapeNode:
+		t.carriageReturnEscape()
+	case *ast.WordCharClassNode:
+		t.wordCharClass(true)
+	case *ast.NotWordCharClassNode:
+		t.notWordCharClass(true)
+	case *ast.DigitCharClassNode:
+		t.digitCharClass()
+	case *ast.NotDigitCharClassNode:
+		t.notDigitCharClass()
+	case *ast.WhitespaceCharClassNode:
+		t.whitespaceCharClass(true)
+	case *ast.NotWhitespaceCharClassNode:
+		t.notWhitespaceCharClass()
+	case *ast.HorizontalWhitespaceCharClassNode:
+		t.horizontalWhitespaceCharClass()
+	case *ast.NotHorizontalWhitespaceCharClassNode:
+		t.notHorizontalWhitespaceCharClass()
+	case *ast.VerticalWhitespaceCharClassNode:
+		t.verticalWhitespaceCharClass()
+	case *ast.NotVerticalWhitespaceCharClassNode:
+		t.notVerticalWhitespaceCharClass()
+	}
+}
+
+func (t *transpiler) namedCharClass(node *ast.NamedCharClassNode) {
+	t.Buffer.WriteString(`[:`)
+	if node.Negated {
+		t.Buffer.WriteRune('^')
+	}
+
+	t.Buffer.WriteString(node.Name)
+	t.Buffer.WriteString(`:]`)
+}
+
+func (t *transpiler) charRange(node *ast.CharRangeNode) {
+	t.charClassElement(node.Left)
+	t.Buffer.WriteRune('-')
+	t.charClassElement(node.Right)
+}
+
+func (t *transpiler) metaCharEscape(node *ast.MetaCharEscapeNode) {
+	t.Buffer.WriteRune('\\')
+	t.Buffer.WriteRune(node.Value)
+}
+
+func (t *transpiler) quotedText(node *ast.QuotedTextNode) {
+	t.Buffer.WriteString(`\Q`)
+	t.Buffer.WriteString(node.Value)
+	t.Buffer.WriteString(`\E`)
+}
+
+func (t *transpiler) caretEscape(node *ast.CaretEscapeNode) {
+	t.Buffer.WriteString(`\x{`)
+	fmt.Fprintf(&t.Buffer, "%x", asciiLetterIndex(node.Value))
+	t.Buffer.WriteString(`}`)
+}
+
+func (t *transpiler) unicodeEscape(node *ast.UnicodeEscapeNode) {
+	t.Buffer.WriteString(`\x{`)
+	t.Buffer.WriteString(node.Value)
+	t.Buffer.WriteString(`}`)
+}
+
+func (t *transpiler) hexEscape(node *ast.HexEscapeNode) {
+	t.Buffer.WriteString(`\x{`)
+	t.Buffer.WriteString(node.Value)
+	t.Buffer.WriteString(`}`)
+}
+
+func (t *transpiler) octalEscape(node *ast.OctalEscapeNode) {
+	t.Buffer.WriteRune('\\')
+	t.Buffer.WriteString(fmt.Sprintf("%03s", node.Value))
+}
+
+func (t *transpiler) unicodeCharClass(node *ast.UnicodeCharClassNode) {
+	t.Buffer.WriteRune('\\')
+	if node.Negated {
+		t.Buffer.WriteRune('P')
+	} else {
+		t.Buffer.WriteRune('p')
+	}
+
+	t.Buffer.WriteRune('{')
+	t.Buffer.WriteString(node.Value)
+	t.Buffer.WriteRune('}')
+}
+
+func (t *transpiler) bellEscape() {
+	t.Buffer.WriteString(`\a`)
+}
+
+func (t *transpiler) formFeedEscape() {
+	t.Buffer.WriteString(`\f`)
+}
+
+func (t *transpiler) tabEscape() {
+	t.Buffer.WriteString(`\t`)
+}
+
+func (t *transpiler) newlineEscape() {
+	t.Buffer.WriteString(`\n`)
+}
+
+func (t *transpiler) carriageReturnEscape() {
+	t.Buffer.WriteString(`\r`)
+}
+
+func (t *transpiler) startOfStringAnchor() {
+	t.Buffer.WriteRune('^')
+}
+
+func (t *transpiler) endOfStringAnchor() {
+	t.Buffer.WriteRune('$')
+}
+
+func (t *transpiler) absoluteStartOfStringAnchor() {
+	t.Buffer.WriteString(`\A`)
+}
+
+func (t *transpiler) absoluteEndOfStringAnchor() {
+	t.Buffer.WriteString(`\z`)
+}
+
+func (t *transpiler) wordBoundaryAnchor() {
+	t.Buffer.WriteString(`\b`)
+}
+
+func (t *transpiler) notWordBoundaryAnchor() {
+	t.Buffer.WriteString(`\B`)
+}
+
+func (t *transpiler) wordCharClass(inCharClass bool) {
+	if t.AsciiMode {
+		t.Buffer.WriteString(`\w`)
+		return
+	}
+
+	if inCharClass {
+		t.Buffer.WriteString(`\p{L}\p{N}_`)
+		return
+	}
+	t.Buffer.WriteString(`[\p{L}\p{N}_]`)
+}
+
+func (t *transpiler) notWordCharClass(inCharClass bool) {
+	if t.AsciiMode {
+		t.Buffer.WriteString(`\W`)
+		return
+	}
+
+	if inCharClass {
+		t.Buffer.WriteString(`[^\p{L}\p{N}_]`)
+	}
+	t.Buffer.WriteString(`^\p{L}\p{N}_`)
+}
+
+func (t *transpiler) digitCharClass() {
+	if t.AsciiMode {
+		t.Buffer.WriteString(`\d`)
+	} else {
+		t.Buffer.WriteString(`\p{Nd}`)
+	}
+}
+
+func (t *transpiler) notDigitCharClass() {
+	if t.AsciiMode {
+		t.Buffer.WriteString(`\D`)
+	} else {
+		t.Buffer.WriteString(`\P{Nd}`)
+	}
+}
+
+func (t *transpiler) whitespaceCharClass(inCharClass bool) {
+	if t.AsciiMode {
+		if inCharClass {
+			t.Buffer.WriteString(`\s\v`)
+			return
+		}
+
+		t.Buffer.WriteString(`[\s\v]`)
+		return
+	}
+
+	if inCharClass {
+		t.Buffer.WriteString(`\s\v\p{Z}\x85`)
+		return
+	}
+	t.Buffer.WriteString(`[\s\v\p{Z}\x85]`)
+}
+
+func (t *transpiler) notWhitespaceCharClass() {
+	if t.AsciiMode {
+		t.Buffer.WriteString(`[^\s\v]`)
+	} else {
+		t.Buffer.WriteString(`[^\s\v\p{Z}\x85]`)
+	}
+}
+
+func (t *transpiler) horizontalWhitespaceCharClass() {
+	if t.AsciiMode {
+		t.Buffer.WriteString(`[\t ]`)
+	} else {
+		t.Buffer.WriteString(`[\t\p{Zs}]`)
+	}
+}
+
+func (t *transpiler) notHorizontalWhitespaceCharClass() {
+	if t.AsciiMode {
+		t.Buffer.WriteString(`[^\t ]`)
+	} else {
+		t.Buffer.WriteString(`[^\t\p{Zs}]`)
+	}
+}
+
+func (t *transpiler) verticalWhitespaceCharClass() {
+	if t.AsciiMode {
+		t.Buffer.WriteString(`[\n\v\f\r]`)
+	} else {
+		t.Buffer.WriteString(`[\n\v\f\r\x85\x{2028}\x{2029}]`)
+	}
+}
+
+func (t *transpiler) notVerticalWhitespaceCharClass() {
+	if t.AsciiMode {
+		t.Buffer.WriteString(`[^\n\x0b\f\r]`)
+	} else {
+		t.Buffer.WriteString(`[^\n\x0b\f\r\x85\x{2028}\x{2029}]`)
+	}
+}
+
+func (t *transpiler) anyCharClass() {
+	t.Buffer.WriteRune('.')
 }
